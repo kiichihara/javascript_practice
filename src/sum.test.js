9f2757e5d2eadcd28d440d34c15977c14e2dee39
kiichihara <122848771+kiichihara@users.noreply.github.com>
2023-03-28 14:11:43 +0900

    20230221

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,75 @@
 const sum = require('./sum');
-
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+test('プログラムコメントの練習', () => {
+//エラーになる為、実行させないようにコメントにする。
+/*
+  expect(sum(1, 2)).toBe(4);
+*/
+});
+test('constの練習', () => {
+const bookTitle = "本";
+const bookPrice = 3000;
+expect(bookTitle).toBe("本");
+expect(bookPrice).toBe(3000);
+});
+test('letの練習', () => {
+let bookTitle;
+expect(bookTitle).toBe(undefined);
+let bookPrice = 3000;
+bookTitle="本";
+expect(bookTitle).toBe("本");
+expect(bookPrice).toBe(3000);
+bookPrice=100;
+expect(bookPrice).toBe(100);
+});
+test('console.logの練習', () => {
+  const total=42+42;
+  console.log(total); // => 84
+  const value = "値";
+  console.log(value); 
+  
+});
+test('文字列の練習', () => {
+ const str1="もじ";
+ const str2='もじ';
+ expect(str1).toBe(str2);
+ expect(str1===str2).toBe(true);
+ const str3=`あああ
+いいい
+ う`;
+ const str4="あああ\nいいい\n う";
+ expect(str3===str4).toBe(true);
+ const str5="これは${str1}です";
+ const str6=`これは${str1}です`;
+ expect(str5).toBe("これは${str1}です");
+ expect(str6).toBe("これはもじです");
+});
+
+test('オブジェクトの練習', () => {
+ const object={
+  address:"東京",
+  telephone:"00-0000-0000",
+  age:20,
+  hasLicense:true,
+  nested:{valuea:1},
+ }
+
+ expect(object.address).toBe("東京");
+ expect(object["telephone"]).toBe("00-0000-0000");
+ expect(object.age).toBe(20);
+ expect(object.hasLicense).toBe(true);
+ expect(object.nested.valuea).toBe(1);
+});
+
+
+ test('配列の練習', () => {
+ const hairetu=["aaa",111,true,{age:20,address:"東京"}]
+
+ expect(hairetu[0]).toBe("aaa");
+ expect(hairetu[1]).toBe(111);
+ expect(hairetu[2]).toBe(true);
+ expect(hairetu[3].address).toBe("東京");
+ expect(hairetu[3].age).toBe(20);
 });
