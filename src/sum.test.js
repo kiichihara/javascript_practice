25701a49c31848f575d90860a77771795295146d
kiichihara <122848771+kiichihara@users.noreply.github.com>
2023-03-28 14:43:24 +0900

    20230328

--- src/sum.test.js
+++ src/sum.test.js
@@ -2,37 +2,22 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
-
-
 test('プログラムコメントの練習', () => {
 //エラーになる為、実行させないようにコメントにする。
 /*
   expect(sum(1, 2)).toBe(4);
 */
 });
-
 test('constの練習', () => {
 const bookTitle = "本";
 const bookPrice = 3000;
 expect(bookTitle).toBe("本");
 expect(bookPrice).toBe(3000);
 });
-
-
-test('constの練習', () => {
-const bookTitle = "本";
-const bookPrice = 3000;
-
-expect(bookTitle).toBe("本");
-expect(bookPrice).toBe(3000);
-});
-
-
 test('letの練習', () => {
 let bookTitle;
 expect(bookTitle).toBe(undefined);
 let bookPrice = 3000;
-
 bookTitle="本";
 expect(bookTitle).toBe("本");
 expect(bookPrice).toBe(3000);
@@ -42,45 +27,19 @@ expect(bookPrice).toBe(100);
 test('console.logの練習', () => {
   const total=42+42;
   console.log(total); // => 84
-
-bookTitle="本";
-expect(bookTitle).toBe("本");
-expect(bookPrice).toBe(3000);
-
-bookPrice=100;
-expect(bookPrice).toBe(100);
-});
-
-test('console.logの練習', () => {
-  const total=42+42;
-  console.log(total); // => 84
-
-
   const value = "値";
   console.log(value); 
   
 });
-
 test('文字列の練習', () => {
  const str1="もじ";
  const str2='もじ';
  expect(str1).toBe(str2);
  expect(str1===str2).toBe(true);
-
-
-test('文字列の練習', () => {
- const str1="もじ";
- const str2='もじ';
-
- expect(str1).toBe(str2);
- expect(str1===str2).toBe(true);
-
-
  const str3=`あああ
 いいい
  う`;
  const str4="あああ\nいいい\n う";
-
  expect(str3===str4).toBe(true);
  const str5="これは${str1}です";
  const str6=`これは${str1}です`;
@@ -133,33 +92,62 @@ test('分割代入 オブジェクトの練習', () => {
  expect(key2).toBe("aaa");
  expect({key,key2}).toStrictEqual(obj);
 });
-
 test('三項演算子の練習', () => {
-
   expect(typeof "文字列").toBe("string");
   expect(typeof undefined).toBe("undefined");
   expect(typeof true).toBe("boolean");
-
   expect(addPrefix("abc")).toBe("デフォルト:abc");
   expect(addPrefix("abc","d")).toBe("dabc");
-
   expect(addPrefix(123)).toBe("デフォルト:123");
   expect(addPrefix("てきすと","プレフィックス")).toBe("プレフィックスてきすと");
 });
-
  function addPrefix(text, prefix) {
   // `prefix`が指定されていない場合は"デフォルト:"を付ける
   const pre = typeof prefix === "string" ? prefix : "デフォルト:";
   return pre + text;
 }
+test('Number.parseInt,Number.parseFloatの練習', () => {
+  expect(Number.parseInt("10",10)).toBe(10);
+  expect(Number.parseInt("10",10)).not.toBe("10");
+  expect(Number.parseInt("10")).toBe(10);
+  expect(Number.parseFloat("3.14",10)).toBe(3.14);
+  expect(Number.parseFloat("3.14",10)).not.toBe("3.14");
+  expect(Number.parseFloat("3.14")).toBe(3.14);
+  expect(Number.parseInt("3.99",10)).toBe(3);
+  expect(Number.parseInt("AAA",10)).toBe(NaN);
+  expect(Number.isNaN(Number.parseInt("AAA",10))).toBe(true);
+});
 
+test('functionの練習', () => {
 
- expect(str3===str4).toBe(true);
+function double(num){
+  return num*2;
+} 
 
- const str5="これは${str1}です";
- const str6=`これは${str1}です`;
+expect(double(10)).toBe(20);
 
- expect(str5).toBe("これは${str1}です");
- expect(str6).toBe("これはもじです");
+function echo(x){
+  return x;
+}
+
+expect(echo(10)).toBe(10);
+expect(echo("10A")).toBe("10A");
+expect(echo()).toBe(undefined);
+});
+
+test('デフォルト引数の練習', () => {
+ function addPrefix2(text, prefix="デフォルト:") {
+  // `prefix`が指定されていない場合は"デフォルト:"を付ける
+
+  //デフォルト関数なので以下は不要になる。
+  //const pre = typeof prefix === "string" ? prefix : "デフォルト:";
+  return prefix + text;
+}
+
+expect(addPrefix2("abc")).toBe("デフォルト:abc");
+expect(addPrefix2("abc","d")).toBe("dabc");
+
+expect(addPrefix2(123)).toBe("デフォルト:123");
+expect(addPrefix2("てきすと","プレフィックス")).toBe("プレフィックスてきすと");
 });
 
